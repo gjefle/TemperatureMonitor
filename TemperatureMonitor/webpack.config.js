c393bd3f137689f66e43eca55875b4c0d75c2e0e
Gjermund Fleten <Gjermund Fleten>
2017-04-02 17:05:00 +0200

    Add project files.

---
+++ TemperatureMonitor/webpack.config.js
@@ -0,0 +1,21 @@
+///// <binding />
+//module.exports = {
+//    entry: './src/main.ts',
+//    output: {
+//        filename: 'bundle.js',
+//        path: __dirname +  "/wwwroot/"
+//    },
+//    module: {
+//        rules: [
+//            {
+//                test: /\.tsx?$/,
+//                loader: 'ts-loader',
+//                exclude: /node_modules/,
+//            }
+//        ]
+//    },
+//    resolve: {
+//        extensions: [".tsx", ".ts", ".js"]
+//    }
+
+//};
