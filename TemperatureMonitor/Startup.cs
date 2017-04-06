c10f0535cdfbbcabfaf9347bde7fb5e055de9b9e
Gjermund Fleten <gjermufl@gmail.com>
2017-04-06 20:50:27 +0200

    moved websocketmanager

--- TemperatureMonitor/Startup.cs
+++ TemperatureMonitor/Startup.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebSocketManager;
 
 namespace TemperatureMonitor
 {
@@ -24,35 +27,33 @@ namespace TemperatureMonitor
         }
 
         public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
             app.UseWebSockets();
+            app.MapWebSocketManager("/temperature", serviceProvider.GetService<TemperatureChangeHandler>());
             app.UseDefaultFiles();
-            app.Use(async (context, next) =>
-            {
-                await next();
+            //app.Use(async (context, next) =>
+            //{
+            //    await next();
 
-                if (context.Response.StatusCode == 404
-                    && !Path.HasExtension(context.Request.Path.Value))
-                {
-                    context.Request.Path = "/index.html";
-                    await next();
-                }
-            });
-
+            //    if (context.Response.StatusCode == 404
+            //        && !Path.HasExtension(context.Request.Path.Value))
+            //    {
+            //        context.Request.Path = "/index.html";
+            //        await next();
+            //    }
+            //});
             app.UseStaticFiles();
-            app.UseMvc();
+            //app.UseMvc();
         }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc();
+            services.AddWebSocketManager();
+        }
+
     }
 }
